986466723f748d43e71a7e5dab3b7d78a7829783
sodawei <wx521kimo@hotmail.com>
2023-02-10 10:45:29 +0800

    初次上傳
    第一次全部上傳

---
+++ js/all.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+   
+    // 漢堡icon 收合
+    $('.bt-toggle').click(function (event) { 
+        event.preventDefault();
+        $('.menu').toggleClass('active');
+        $('.menu').slideToggle();
+
+    });
+
+    // 遮title字黑板出現
+    $('.bt-toggle')
+    .click(function() {
+        $('.menu-screen').fadeToggle(500);
+     })
+
+
+    $(window).resize(function () {
+        const width = $(window).width();
+        if (width > "375") { // 視窗的寬度大於 1024px ( 電腦斷點 ) 
+          $(".menu").removeAttr("style"); 
+          //強制把 slideToggle 加上的 style 屬性清除
+
+          $(".menu-screen").removeAttr("style"); 
+        }
+      });
+});
